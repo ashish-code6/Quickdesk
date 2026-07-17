e2b0ffc9d3fb3834eeda6012d21be9d873cf9dac
ashish-code6 <ashishk810222@gmail.com>
2026-07-17 12:51:54 +0530

    Implement Protected Routes

--- quickdesk-frontend/src/App.jsx
+++ quickdesk-frontend/src/App.jsx
@@ -3,17 +3,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
   return (
-   
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    
+
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+
   );
 }
 
--- quickdesk-frontend/src/pages/Dashboard.jsx
+++ quickdesk-frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,83 @@
-const Dashboard=()=>{
-    return(
-        <>
-            <h1>Welcome to Dashboard Page</h1>
-        </>
-    )
-}
+import { useEffect, useState } from "react";
+import api from "../services/api";
+import toast from "react-hot-toast";
+
+
+const Dashboard = () => {
+
+  const [message, setMessage] = useState("");
+
+
+  useEffect(() => {
+
+    const getProfile = async () => {
+
+      try {
+
+        const { data } = await api.get(
+          "/users/profile"
+        );
+
+        setMessage(data.message);
+
+
+      } catch (error) {
+
+        toast.error("Session expired");
+
+      }
+
+    };
+
+
+    getProfile();
+
+  }, []);
+
+
+
+  const logout = () => {
+
+    localStorage.removeItem("token");
+
+    window.location.href = "/login";
+
+  };
+
+
+  return (
+
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+
+
+      <div className="bg-white p-10 rounded-2xl shadow-lg text-center">
+
+
+        <h1 className="text-3xl font-bold text-slate-800">
+          Welcome to QuickDesk Dashboard 👋
+        </h1>
+
+
+        <p className="mt-4 text-slate-500">
+          {message}
+        </p>
+
+
+        <button
+          onClick={logout}
+          className="mt-6 rounded-xl bg-red-500 px-6 py-3 text-white font-semibold hover:bg-red-600"
+        >
+          Logout
+        </button>
+
+
+      </div>
+
+
+    </div>
+
+  );
+};
+
+
 export default Dashboard;
